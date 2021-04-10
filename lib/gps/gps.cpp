b89c6a76b304843734bde714dff21cd8b3e724bc
foorschtbar <mail@wischmopp90.com>
2021-04-10 22:15:33 +0200

    Renamed GPS dir

---
+++ lib/gps/gps.cpp
@@ -0,0 +1,79 @@
+#include "gps.h"
+
+HardwareSerial GPSSerial(1);
+
+byte CFG_RST[12] = {0xb5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0F, 0x66};
+
+void Gps::init()
+{
+  GPSSerial.begin(9600, SERIAL_8N1, GPS_TX, GPS_RX);
+  GPSSerial.setTimeout(2);
+  GPSSerial.write(CFG_RST, sizeof(CFG_RST)); // Soft Reset GPS on Start
+  satsInView.begin(tGps, "GPGSV", 3);        // NMEA Sentence GPGSV, Element 3 (Sat in View)
+}
+
+void Gps::encode()
+{
+  int previousMillis = millis();
+
+  while ((previousMillis + 1000) > millis())
+  {
+    while (GPSSerial.available())
+    {
+      char data = GPSSerial.read();
+      tGps.encode(data);
+    }
+  }
+}
+
+bool Gps::buildPacket(uint8_t txBuffer[9])
+{
+  LatitudeBinary = ((tGps.location.lat() + 90) / 180.0) * 16777215;
+  LongitudeBinary = ((tGps.location.lng() + 180) / 360.0) * 16777215;
+
+  txBuffer[0] = (LatitudeBinary >> 16) & 0xFF;
+  txBuffer[1] = (LatitudeBinary >> 8) & 0xFF;
+  txBuffer[2] = LatitudeBinary & 0xFF;
+
+  txBuffer[3] = (LongitudeBinary >> 16) & 0xFF;
+  txBuffer[4] = (LongitudeBinary >> 8) & 0xFF;
+  txBuffer[5] = LongitudeBinary & 0xFF;
+
+  altitudeGps = tGps.altitude.meters();
+  txBuffer[6] = (altitudeGps >> 8) & 0xFF;
+  txBuffer[7] = altitudeGps & 0xFF;
+
+  hdopGps = tGps.hdop.value() / 10;
+  txBuffer[8] = hdopGps & 0xFF;
+
+  return true;
+}
+
+void Gps::gdisplay(uint16_t txBuffer2[6])
+{
+  txBuffer2[0] = tGps.satellites.value();  // Number of satellites in use (u32)
+  txBuffer2[1] = tGps.speed.kmph();        // Speed in kilometers per hour (double)
+  txBuffer2[2] = tGps.course.deg();        // Course in degrees (double)
+  txBuffer2[3] = tGps.altitude.meters();   // Altitude in meters (double)
+  txBuffer2[4] = tGps.hdop.value() / 10;   // Horizontal Dim. of Precision (100ths-i32)
+  txBuffer2[5] = atoi(satsInView.value()); // NMEA Sentence GPGSV, Element 3 (Sat in View)
+}
+
+bool Gps::checkGpsFix()
+{
+  encode();
+  if (tGps.location.isValid() &&
+      tGps.location.age() < 2000 &&
+      tGps.hdop.isValid() &&
+      tGps.hdop.value() <= 300 &&
+      tGps.hdop.age() < 2000 &&
+      tGps.altitude.isValid() &&
+      tGps.altitude.age() < 2000)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
